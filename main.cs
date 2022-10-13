478fc1751a78f2c86e4b4c79f0a1c0e44b1338ee
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-13 19:11:42 -0400

    Code Change

--- main.cs
+++ main.cs
@@ -10,7 +10,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // This function accepts user input
+        string streetName;
+        int streetNumber;
+
+        Console.WriteLine("This program gets shows the address you put in");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your address is: " + streetNumber + " " + streetName + ".");
+
         Console.WriteLine("\nDone.");
     }
 }
